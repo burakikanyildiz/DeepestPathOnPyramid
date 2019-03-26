866ea9d4b5f322f96641654aa6676f9f70b9eacf
Burak İkan Yıldız <burakikanyildiz@gmail.com>
2019-03-27 02:44:03 +0300

    Algorithm correction

--- pyramid.py
+++ pyramid.py
@@ -1,21 +1,17 @@
 import sys
 
-def isPrime(n) : 
-  
-    if (n <= 1) : 
+def isPrime(n): 
+    if n <= 1: 
         return False
-    if (n <= 3) : 
+    if n <= 3: 
         return True
-  
-    if (n % 2 == 0 or n % 3 == 0) : 
+    if n % 2 == 0 or n % 3 == 0: 
         return False
-  
     i = 5
-    while(i * i <= n) : 
-        if (n % i == 0 or n % (i + 2) == 0) : 
+    while i*i <= n: 
+        if n % i == 0 or n % (i + 2) == 0: 
             return False
         i = i + 6
-  
     return True
 
 if len(sys.argv)!=2:
@@ -31,36 +27,17 @@ for line in content:
   lines.append(line.split(' '))
 numberOfLines=len(lines)
 
-def bestPath(lineNumber,index):#Traversing possible paths recursively
-  global numberOfLines
-  global lines
-  if lineNumber==0:
-    if not isPrime(int(lines[0][0])):
-      return int(lines[0][0])+bestPath(lineNumber+1,index)[0],lineNumber
-  elif lineNumber>=numberOfLines:
-    return 0,lineNumber
-  else:
-    first=int(lines[lineNumber][index])
-    second=int(lines[lineNumber][index+1])
-    if not isPrime(first) and not isPrime(second):
-      x=bestPath(lineNumber+1,index)
-      y=bestPath(lineNumber+1,index+1)
-      if x and y:
-        if int(x[0])>int(y[0]):
-          return first+bestPath(lineNumber+1,index)[0],lineNumber
-        elif int(x[0])<int(y[0]):
-          return second+bestPath(lineNumber+1,index+1)[0],lineNumber
-        else:
-          return max(first+bestPath(lineNumber+1,index)[0],second+bestPath(lineNumber+1,index+1)[0]),lineNumber
-      elif x:
-        return first+bestPath(lineNumber+1,index)[0],lineNumber
-      elif y:
-        return second+bestPath(lineNumber+1,index+1)[0],lineNumber
-    elif not isPrime(first):
-      return first+bestPath(lineNumber+1,index)[0],lineNumber
-    elif not isPrime(second):
-      return second+bestPath(lineNumber+1,index+1)[0],lineNumber
-    else:
-      return 0,lineNumber
+table=[[0]*len(lines) for j in range(len(lines))]
 
-print(bestPath(0,0)[0])
+for i in range(len(lines)):
+  for j in range(len(lines[i])):
+    if isPrime(int(lines[i][j])):
+      table[i][j]=-99999999
+    else:
+      table[i][j]=int(lines[i][j])
+for i in range(len(lines)-2,-1,-1):
+  for j in range(i,-1,-1):
+    if table[i][j]!=-99999999:
+      table[i][j]=max(table[i][j]+table[i+1][j],table[i][j]+table[i+1][j+1])
+
+print(table[0][0])
